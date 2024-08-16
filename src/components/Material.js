9caed70a4148929076000a4b515bd615198c1dfd
vasusharrma <sharmavashu099@gmail.com>
2024-08-16 18:24:53 +0530

    all

---
+++ src/components/Material.js
@@ -0,0 +1,16 @@
+import './Material.css';
+
+
+function Material(){
+
+    return(
+        <div className='material_outside'>
+            <button className='minus_btn' >-</button> 
+            <p>0</p>
+            <button className="plus_btn">+</button>
+        </div>
+    )
+}
+
+
+export default Material;
