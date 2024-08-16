d0f8e67127f3824e36734d77c81977584327448c
vasusharrma <sharmavashu099@gmail.com>
2024-08-16 19:08:32 +0530

    completed

--- src/components/Material.js
+++ src/components/Material.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import './Material.css';
-
 
 function Material(){
 
+    
+
+    const [currValue , setValue] = useState(0);
+
+    function decBtnHandler(){
+        setValue(currValue-1);
+    }
+
+    function incBtnHandler(){
+        setValue(currValue+1);
+    }
+
     return(
         <div className='material_outside'>
-            <button className='minus_btn' >-</button> 
-            <p>0</p>
-            <button className="plus_btn">+</button>
+            <button className='minus_btn' onClick={decBtnHandler} >-</button> 
+            <p>{currValue}</p>
+            <button className="plus_btn" onClick={incBtnHandler}>+</button>
         </div>
     )
 }
