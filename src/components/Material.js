3d1f1968c07c45b740a76951b666812678e5652a
vasusharrma <sharmavashu099@gmail.com>
2024-08-16 23:19:29 +0530

    final changes

--- src/components/Material.js
+++ src/components/Material.js
@@ -1,28 +1,37 @@
-import { useState } from 'react';
-import './Material.css';
+import { useState } from "react";
+import "./Material.css";
 
-function Material(){
+function Material() {
+  const [currValue, setValue] = useState(0);
 
-    
+  function decBtnHandler() {
+    setValue(currValue - 1);
+  }
 
-    const [currValue , setValue] = useState(0);
+  function incBtnHandler() {
+    setValue(currValue + 1);
+  }
 
-    function decBtnHandler(){
-        setValue(currValue-1);
-    }
+  function resBtnHandler(){
+    setValue(0);
+  }
 
-    function incBtnHandler(){
-        setValue(currValue+1);
-    }
 
-    return(
-        <div className='material_outside'>
-            <button className='minus_btn' onClick={decBtnHandler} >-</button> 
-            <p>{currValue}</p>
-            <button className="plus_btn" onClick={incBtnHandler}>+</button>
-        </div>
-    )
+  return (
+    <div className="material_outside">
+      <p id="material_para">Increment & Decrement</p>
+      <div className="material_box">
+        <button className="minus_btn material_btn" onClick={decBtnHandler}>
+          -
+        </button>
+        <p class="material_para">{currValue}</p>
+        <button className="plus_btn material_btn" onClick={incBtnHandler}>
+          +
+        </button>
+      </div>
+      <button className="material_reset_btn" onClick={resBtnHandler}>Reset</button>
+    </div>
+  );
 }
-
 
 export default Material;
